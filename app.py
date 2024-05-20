e508cd0b2705eccdcf336561dc65aa47b98fd389
CharanPatgar <charanpatgar19@gmail.com>
2024-05-21 00:17:04 +0530

    made a page for 43 traffic signs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import subprocess
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,6 +10,10 @@ users = {
     "user1": "password1",
     "user2": "password2"
 }
+
+# Load class names from CSV
+class_names = pd.read_csv('labels.csv', index_col=0).iloc[:, 0].to_dict()
+images_path = 'images'  # Directory where traffic sign images are stored
 
 @app.route('/')
 def login():
@@ -29,8 +35,15 @@ def dashboard():
 
 @app.route('/run-test')
 def run_test():
-    subprocess.Popen(["python", "Test.py"])  # Change the path as needed
+    subprocess.Popen(["python", "test.py"])  # Change the path as needed
     return 'Test.py executed successfully!'
+
+@app.route('/traffic-signs')
+def traffic_signs():
+    images_path = os.path.join(app.static_folder, 'images')
+    images = [img for img in os.listdir(images_path) if os.path.isfile(os.path.join(images_path, img))]
+    traffic_signs = [{'name': os.path.splitext(img)[0], 'image': f'images/{img}'} for img in images]
+    return render_template('traffic_signs.html', traffic_signs=traffic_signs)
 
 if __name__ == '__main__':
     app.run(debug=True)
